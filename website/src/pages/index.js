04e673e086c755b733d5982d4f49984634f7dfdc
Mikers <snissn@gmail.com>
2025-07-29 13:49:33 -1000

    fix broken links

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started-loading-your-first-agent-file">
+            to="/tutorials/getting-started-loading-your-first-agent-file">
             Agents Hub Tutorial - 5min ⏱️
           </Link>
         </div>
