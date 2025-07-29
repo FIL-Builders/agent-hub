f72944ffe98c243f4f2fcf2af76ab3b7f30d65af
Mikers <snissn@gmail.com>
2025-07-29 10:39:52 -1000

    website progress

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -100,9 +100,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Agent Hub',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Agent Hub Logo',
           src: 'img/logo.svg',
         },
         items: [
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/getting-started-loading-your-first-agent-file">
+            Agents Hub Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
