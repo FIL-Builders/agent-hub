2ccdf792c6fd677eebe550146e774aea74ada789
Mikers <snissn@gmail.com>
2025-09-23 11:58:52 +0530

    MCP: broaden CORS allow-methods to GET, POST, OPTIONS, HEAD

--- netlify/functions/mcp.js
+++ netlify/functions/mcp.js
@@ -202,7 +202,7 @@ async function listAllTools() {
 function corsHeaders() {
   return {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, HEAD",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
   };
 }
